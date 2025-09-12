df68a2d6fea137e0873ede4a5a3d6a2965207288
walidayankawsar <walidayankawsar@gmail.com>
2025-09-12 05:15:29 -0700

    created porject

---
+++ getRegistration/getRegistration/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for getRegistration project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'getRegistration.settings')
+
+application = get_asgi_application()
