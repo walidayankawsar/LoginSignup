f51a775e7a50ff401f8ae0cd2fe921ce8410a695
walidayankawsar <walidayankawsar@gmail.com>
2025-09-13 01:51:11 -0700

    updated

--- getRegistration/home/urls.py
+++ getRegistration/home/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
 ]
