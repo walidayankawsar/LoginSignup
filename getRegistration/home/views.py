9a7a415fa53eaace99b0d83cb0eebde2a4c96341
walidayankawsar <walidayankawsar@gmail.com>
2025-09-14 11:05:58 -0700

    fixed save() function

--- getRegistration/home/views.py
+++ getRegistration/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
@@ -12,7 +12,8 @@ def signup(request):
     if request.method == 'POST':
         form =UserCreationForm(request.POST or None)
         if form.is_valid():
-            form.save
+            form.save()
+            return redirect('login') 
     else: 
         form = UserCreationForm()
     return render(request, 'registration/signup.html', {'form': form})
